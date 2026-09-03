e232df0ae3afcc40ee131a41351385c1cdc949ab
anupamdattaiitr01 <anuuulonewolf2001@gmail.com>
2026-09-03 19:50:36 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map <int, int> mpp;
+        for (int i =0;i<nums.size();i++)
+        {
+            if (mpp.find (nums[i]) != mpp.end()) return nums[i];
+            mpp[nums[i]]++;
+        }
+
+        return -1;
+    }
+};
